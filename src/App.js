2caf44bbc8a09b434c0fcc1f0ecd57ce3d20e5cb
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2022-08-09 10:42:17 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <Dropdown />
       </div>
       <div className="designBy">
-      <p className="day">Day 1</p>
+        <p className="day">Day 1</p>
         <h1>By</h1>
         <p> Mustafa Karaçuha</p>
       </div>
